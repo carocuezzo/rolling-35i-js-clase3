9aceb4bc694ed300f3a6316b9c197c61e1d29fd4
Fernando del Valle <fernandomatiasdv@gmail.com>
2022-12-01 21:13:59 -0300

    agregue teoria condicionales

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 let numero = prompt("Ingresa un numero")
 let numeroFormateado = parseInt(numero)
 console.log(typeof numero)
@@ -9,7 +10,7 @@ let multiplicacion = numeroFormateado * 3
 let division = numeroFormateado / 2
 let divisionProhibida  = numeroFormateado / 0
 let modulo = numeroFormateado % 4
-/*
+
 El 0 es el módulo o resto:
 16  |__4___
  0/    4
@@ -17,7 +18,6 @@ El 0 es el módulo o resto:
 El 3 es el módulo o resto:
 19  |__4___
  3/    4
-*/
 console.log(Math)
 
 console.log("Resultado " + resultado)
@@ -32,5 +32,96 @@ console.log(`División: ${division}`)
 
 console.log(`División prohibida: ${divisionProhibida}`)
 console.log(`Resto: ${modulo}`)
+
+
+
+// 4. Sabiendo que la función f(x)=3x^2+5x+2 viene dada por un x obtenido de un prompt,
+//    mostrar el valor resultante de dicha función por consola.
+
+let valorX = prompt("Dale un valor a X")
+let x = parseInt(valorX)
+
+let resultado = 3*x*(3*x)+5*x+2
+console.log(resultado)
+
+
+let edad = 14
+if(edad >= 18)
+{
+    alert("Pudiste ingresar a la PFA!!")
+}else
+{
+    alert("Aún te faltan años para ingresar")
+}
+*/
+
+/*
+let horario = "A"
+if(horario < 6){
+    alert("Era de madrugada")
+}else if(horario <= 12){
+    alert("Es la mañana")
+}else if(horario <= 20){
+    alert("Es durante la tarde")
+}else if(horario <= 24){
+    alert("Es durante la noche")
+}
+*/
+/*
+let dia = 1
+let diaParseado = dia.toString()
+if(diaParseado === "1"){
+    alert("Es lunes")
+}else{
+    alert("NO es lunes")
+}
+*/
+
+let dia = 1
+let diaParseado = dia.toString()
+/*if(diaParseado === "1"){
+    alert("Es lunes")
+}else if(diaParseado === "2"){
+    alert("Es martes")
+}else if(diaParseado === "3"){
+    alert("Es miercoles")
+}else{
+    alert("Es otro día")
+}*/
+
+/*
+switch(dia){
+    case 1: 
+        alert("Es lunes");
+        break;
+    case 2: 
+        alert("Es Martes");
+        break;
+    case 3: 
+        alert("Es Miercoles");
+        break;
+    default: alert("Es otro día ")
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
